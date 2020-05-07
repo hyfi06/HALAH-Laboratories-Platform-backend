7d29a19465d82c194c56ef1bce0059dfc5181020
Alexis Araujo <ala1289@gmail.com>
2020-05-06 23:00:31 -0500

    [FIX] Changes config, services and routes

---
+++ components/users/usersService.js
@@ -0,0 +1,22 @@
+const MongoLib = require('../../lib/mongo');
+const { config } = require('../../config');
+
+class usersService {
+  constructor() {
+    this.collection = config.dbCollections.users;
+    this.mongoDB = new MongoLib();
+  }
+
+  async getUsers({ role }) {
+    const query = role && { role: { $in: role } };
+    const users = await this.mongoDB.getAll(this.collection, query);
+    return users || [];
+  }
+
+  async createUsers(users) {
+    const createUsersId = await this.mongoDB.createMany(this.collection, users);
+    return createUsersId;
+  }
+}
+
+module.exports = usersService;
--- src/index.js
+++ src/index.js
@@ -1,9 +1,16 @@
 const express = require('express');
 const app = express();
+const helmet = require('helmet');
 
 const { config } = require('../config/index');
 
+const usersApi = require('../components/users/routes');
+
+app.use(helmet());
 app.use(express.json());
+
+//Routes
+usersApi(app);
 
 app.listen(config.port, function () {
   console.log(`Listening http://localhost:${config.port}`);
