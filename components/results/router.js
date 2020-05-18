da37a05b6010289613178da39527cf6eae820b9d
Héctor Olvera Vital <hector.olvera@ciencias.unam.mx>
2020-05-18 16:38:04 -0500

    feat(results): add results routes
    Add results/router.js
    update package.json
    - upgrade version
    
    Refs #41

---
+++ components/results/router.js
@@ -0,0 +1,56 @@
+const express = require('express');
+const passport = require('passport');
+require('../../utils/auth/strategies/jwt');
+
+const validationIdHandler = require('../../utils/middleware/validationIdHandler');
+
+const ResultsService = require('./resultsService');
+
+function resultsApi(app) {
+  const router = express.Router();
+
+  app.use('/api/results', router);
+
+  const resultsService = new ResultsService();
+
+  router.post(
+    '/',
+    passport.authenticate('jwt', { session: false }),
+    async function (req, res, next) {
+      const { results } = req.body;
+
+      try {
+        const createResultsId = await resultsService.createResult(results);
+
+        res.status(201).json({
+          data: createResultsId,
+          message: 'result created',
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+
+  router.get(
+    '/:resultId',
+    passport.authenticate('jwt', { session: false }),
+    validationIdHandler('resultId'),
+    async function (req, res, next) {
+      const { resultId } = req.params;
+
+      try {
+        const result = await resultsService.getResults(resultId);
+
+        res.status(200).json({
+          data: result,
+          message: 'result retrieved',
+        });
+      } catch (error) {
+        next(error);
+      }
+    }
+  );
+}
+
+module.exports = resultsApi;
